2d1f2dbcbb1b6513688c4182f43adfe36fcfa44b
zonoman <morizono.2st@gmail.com>
2020-01-18 15:50:58 +0900

    toy_app deleted

--- environment/toy_app/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-    has_many :microposts
-    validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
-    validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
-end
