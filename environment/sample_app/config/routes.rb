84acf17293a4b4ab77d37ebaeb3b3e374a4d989d
zonoman <morizono.2st@gmail.com>
2020-01-19 17:50:37 +0900

    signuppage_created

--- environment/sample_app/config/routes.rb
+++ environment/sample_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#home'
 
   get '/help', to:'static_pages#help'
@@ -7,7 +8,8 @@ Rails.application.routes.draw do
 
   get '/contact', to:'static_pages#contact'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get '/signup', to:'users#new'
+
+
 
 end
