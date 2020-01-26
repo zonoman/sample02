4890dddd6ccbaf09d0fd23c9a2d7996fb90c571e
zonoman <morizono.2st@gmail.com>
2020-01-26 12:36:05 +0900

    Implement basic login

--- environment/sample_app/app/controllers/sessions_controller.rb
+++ environment/sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      # →user_url(user) と同じ
+    else
+      flash.now[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
 end
--- environment/sample_app/app/helpers/sessions_helper.rb
+++ environment/sample_app/app/helpers/sessions_helper.rb
@@ -1,2 +1,25 @@
 module SessionsHelper
+
+    def log_in(user)
+        session[:user_id] = user.id 
+        #railsで事前定義済みのsessionメソッドに、ユーザーIDを記憶させる。
+        #このコードは自動的に暗号化されて、保護される
+    end
+
+    def current_user
+        if session[:user_id]
+          @current_user = @current_user || User.find_by(id:session[:user_id])
+        end
+      end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def log_out
+      session.delete(:user_id)
+      @current_user = nil
+    end
+    
+
 end
