622b3c1da5af98d4b0c1c83928b831e593b61e4a
zonoman <morizono.2st@gmail.com>
2020-01-27 23:19:26 +0900

    user edit, update, index, and destroy actions

--- environment/sample_app/app/controllers/users_controller.rb
+++ environment/sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_action :logged_in_user,only:[:index,:edit, :update]
+before_action :logged_in_user,only:[:index,:edit, :update,:destroy]
 before_action :correct_user, only:[:edit,:update]
+before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -53,6 +54,11 @@ before_action :correct_user, only:[:edit,:update]
     redirect_to(root_url) unless current_user?(@user) 
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
 
 
@@ -64,6 +70,11 @@ before_action :correct_user, only:[:edit,:update]
       params.require(:user).permit(:name,:email,:password,
                               :password_confirmation)
     end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 
 
 
