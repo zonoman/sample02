86dc1b15530c131aa64b589563b9e189e7cfbffd
zonoman <morizono.2st@gmail.com>
2020-01-30 12:26:46 +0900

    12.2

--- environment/sample_app/config/routes.rb
+++ environment/sample_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   root 'static_pages#home'
@@ -22,7 +26,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations, only: [:edit]
-
+  resources :password_resets, only:[:new,:create,:edit,:update]
 
 
 end
